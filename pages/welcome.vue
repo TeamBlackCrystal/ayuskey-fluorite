ed4ca73c13a483f88eed9e96152e9dd7d80936e3
yupix <yupi0982@outlook.jp>
2021-09-25 02:29:43 +0900

    feat: welcomeで一部を除きインスタンスの情報を表示できるように

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -1,96 +1,179 @@
 <template>
-  <v-container fluid style='padding-left: 15%; padding-right:15%;'>
-    <v-row>
-      <v-col xs='12' sm='8'>
-        <v-card class='rounded-xl pa-5 mb-10' min-height='20em'>
-          <h1>{{ instanceName }}</h1>
-          <v-row class='pa-3'>
-            <p class='pr-7'>{{ host }} - Powered by Ayuskey.</p>
-            <div class='pr-5'>
-              <v-icon small>fas fa-user</v-icon>
-              90
-            </div>
-            <div>
-              <v-icon small>fas fa-pencil-alt</v-icon>
-              5,900
-            </div>
-            <p>Akari NeはActivity Pubを漂っているインスタンスの一つです。基本的にはAkariNextコミュニティーが使用していますが、その他の方も是非ご登録してくださいね！詳しく...
-            </p>
-            <v-btn
-              class='ma-2'
-              outlined
-              color='white'
-            >
-              新規登録
-            </v-btn>
-            <v-btn
-              class='ma-2'
-              outlined
-              color='white'
-            >
-              ログイン
-            </v-btn>
-          </v-row>
+  <div>
+    <div :style='customBanner'
+         style='position:absolute; width: 100%; min-height: 40%; background-size: 100% auto; background-repeat: no-repeat;'></div>
 
-        </v-card>
-        <v-row class='mb-5'>
-          <v-col cols='12' sm='6'>
-            <v-card class='rounded-lg' min-height='27.5em'>
-              <v-app-bar class='rounded-0 rounded-t-lg'>お知らせ</v-app-bar>
-              Second Card
-            </v-card>
-          </v-col>
-          <v-col>
-            <v-card class='rounded-lg' min-height='27.5em'>
-              <v-app-bar class='rounded-0 rounded-t-lg'>最近の画像</v-app-bar>
-              Second Card
-            </v-card>
-          </v-col>
-        </v-row>
-        <v-card class='rounded-lg pa-5 mb-10' min-height='15em'>
-          文字
-        </v-card>
-      </v-col>
-      <v-col xs='12'>
-        <v-card class='rounded-lg mb-10' min-height='32em'>
-          <v-app-bar class='rounded-0 rounded-t-lg'>タイムライン</v-app-bar>
-          Second Card
-        </v-card>
-        <v-card class='rounded-xl pa-5 mb-10' min-height='20em'>
-          タグ
-          <hr/>
-        </v-card>
-        <v-card class='rounded-lg' min-height='10em'>
-          <v-app-bar class='rounded-0 rounded-t-lg'>情報</v-app-bar>
-          <p class='pa-5'>Version: 11.37.1-rei0784-5.16.0<br><span>Maintainer: yupix</span></p>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+    <v-container fluid style='padding-left: 15%; padding-right:15%;'>
+      <v-row>
+        <v-col xs='12' sm='8'>
+          <v-card class='rounded-xl pa-5 mb-10' min-height='20em'>
+            <h1>{{ meta.name }}</h1>
+            <v-row class='pa-3'>
+              <p class='pr-7'>{{ host }} - Powered by Ayuskey.</p>
+              <div class='pr-5'>
+                <v-icon small>fas fa-user</v-icon>
+                {{ stats.originalUsersCount }}
+              </div>
+              <div class='pr-5'>
+                <v-icon small class='light-green--text'>fas fa-user</v-icon>
+                {{ onlineUsersCount }}
+              </div>
+              <div>
+                <v-icon small>fas fa-pencil-alt</v-icon>
+                {{ stats.originalNotesCount }}
+              </div>
+            </v-row>
+            <p>{{ meta.description }}</p>
+            <div>
+              <v-btn
+                class='mr-5'
+                outlined
+                color='white'
+              >
+                新規登録
+              </v-btn>
+              <v-btn
+                outlined
+                color='white'
+              >
+                ログイン
+              </v-btn>
+            </div>
+          </v-card>
+          <v-row class='mb-5'>
+            <v-col cols='12' sm='6'>
+              <v-card class='rounded-lg' min-height='27.5em'>
+                <v-app-bar class='rounded-0 rounded-t-lg elevation-0'>
+                  <v-icon small class='pr-2'>fas fa-broadcast-tower</v-icon>
+                  お知らせ
+                </v-app-bar>
+                <div v-for='announcement in announcements' :key='announcement.id'>
+                  <v-card>
+                    <v-card-title>{{ announcement.title }}</v-card-title>
+                    <v-card-subtitle>{{ announcement.text }}</v-card-subtitle>
+                  </v-card>
+                  <img v-if='announcement.imageUrl' :src='announcement.imageUrl' />
+                </div>
+              </v-card>
+            </v-col>
+            <v-col cols='12' sm='6'>
+              <v-card class='rounded-lg' min-height='27.5em' max-height='27.5em'>
+                <v-app-bar class='rounded-0 rounded-t-lg elevation-0'>
+                  <v-icon small class='pr-2'>far fa-images</v-icon>
+                  最近の画像
+                </v-app-bar>
+                <div class='d-flex flex-wrap align-content-center'>
+                  <v-img v-for='photo in photos' :key='photo' :src='photo.url' width='50%'></v-img>
+                </div>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-card class='rounded-lg pa-5 mb-10' min-height='15em'>
+            文字
+          </v-card>
+        </v-col>
+        <v-col xs='12'>
+          <v-card class='rounded-lg mb-10' min-height='32em'>
+            <v-app-bar class='rounded-0 rounded-t-lg elevation-0'>
+              <v-icon small class='pr-2'>far fa-comment-alt</v-icon>
+              タイムライン
+            </v-app-bar>
+            Second Card
+          </v-card>
+          <v-card class='rounded-xl pa-5 mb-10' min-height='20em'>
+            <v-list three-line>
+              <a v-for='tag in tags' :key='tag' target='_blank' :href='meta.uri + "/tags/" + tag.tag'
+                 class='light-green--text text-decoration-none'> {{ tag.tag }}
+                <v-divider></v-divider>
+              </a>
+            </v-list>
+          </v-card>
+          <v-card class='rounded-lg' min-height='10em'>
+            <v-app-bar class='rounded-0 rounded-t-lg elevation-0'>
+              <v-icon small class='pr-2'>fas fa-info-circle</v-icon>
+              情報
+            </v-app-bar>
+            <p class='pa-5'>Version: {{ meta.version }}<br><span>Maintainer: {{ meta.maintainerName }}</span></p>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 <script lang='ts'>
 import { defineComponent } from '@nuxtjs/composition-api'
-import { toUnicode } from 'punycode/';
-import { host, instanceName } from '~/utils/config';
-import * as os from '~/utils/os';
+import { toUnicode } from 'punycode/'
+import { host, instanceName } from '~/utils/config'
+import * as os from '~/utils/os'
+import { concat } from '~/utils/prelude/array'
 
 export default defineComponent({
   name: 'Welcome',
   data() {
     return {
+      meta: {},
+      stats: {},
+      tags: [],
+      photos: [],
+      announcements: [],
+      onlineUsersCount: 0,
       host: toUnicode(host),
-      instanceName,
-      stats: null,
+      instanceName
+    }
+  },
+  computed: {
+    customBanner() {
+      console.log('あひゃ')
+      console.log(this.meta.bannerUrl)
+      return {
+        'background': 'url(' + this.meta.bannerUrl + ')'
+      }
     }
   },
   created() {
     os.api('stats').then(stats => {
-      this.stats = stats;
+      this.stats = stats
+    })
+
+    os.api('meta').then(meta => {
+      console.log(meta)
+      this.meta = meta
+    })
+
+    os.api('get-online-users-count').then(res => {
+      this.onlineUsersCount = res.count
+    })
+
+    os.api('hashtags/list', {
+      sort: '+mentionedLocalUsers',
+      limit: 8
+    }).then(tags => {
+      this.tags = tags
+    })
+
+    os.api('announcements', {
+      limit: 10
+    }).then(announcements => {
+      this.announcements = announcements
+    })
+
+    const image = [
+      'image/jpeg',
+      'image/png',
+      'image/gif',
+      'image/apng',
+      'image/vnd.mozilla.apng'
+    ]
+
+    os.api('notes/local-timeline', {
+      fileType: image,
+      excludeNsfw: true,
+      limit: 6
+    }).then((notes: any[]) => {
+      console.log(notes)
+      const files = concat(notes.map((n: any): any[] => n.files))
+      this.photos = files.filter(f => image.includes(f.type)).slice(0, 6)
     })
   }
 })
 </script>
-
-<style lang='stylus' scoped>
-
-</style>
