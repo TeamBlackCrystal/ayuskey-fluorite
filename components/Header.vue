8cbd7c212b53d6e04aa203609186deea43bd92f6
yupix <yupi0982@outlook.jp>
2021-09-30 01:09:58 +0900

    feat: headerを部分的に復活

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,51 @@
 <template>
-  <div>
-    <v-app-bar>
+  <div style='position:-webkit-sticky; position:sticky; top:0; z-index: 10;'>
+    <v-app-bar
+      app
+      dark
+      style='position:-webkit-sticky; position:sticky; top:0; z-index: 10:'
+      flat
+    >
+      <v-avatar
+        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
+        size='32'
+      ></v-avatar>
 
+      <v-tabs
+        centered
+        class='ml-n9'
+        color='grey darken-1'
+      >
+        <v-tab
+          v-for='link in links'
+          :key='link'
+        >
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+
+      <v-avatar
+        class='hidden-sm-and-down'
+        size='50'
+      >
+        <v-img :src='i.avatarUrl'></v-img>
+      </v-avatar>
     </v-app-bar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  data() {
+    return {
+      i: {}
+    }
+  },
+  created() {
+    this.i = JSON.parse(localStorage.getItem('account'))
+  }
+
 }
 </script>
 
