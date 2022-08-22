a6f1a4ce994c7bf57d654f0cc86e3d8ba5d087fc
yupix <yupi0982@outlook.jp>
2022-08-22 20:55:00 +0900

    fix: ビルドできない

--- src/hooks/useAyuskeyClient.ts
+++ src/hooks/useAyuskeyClient.ts
@@ -3,18 +3,19 @@ import { useMemo } from "react";
 import { useLocalStorage } from "../store/auth";
 
 type AyuskeyApiOption = {
-  origin: api.APIClient['origin'];
-  credential?: api.APIClient['credential'];
+	origin: api.APIClient["origin"];
+	credential?: api.APIClient["credential"];
 };
 
-
 export const useAyuskeyClient = (options?: AyuskeyApiOption) => {
-  const {mainAccount} = useLocalStorage.getState()
-  if (!mainAccount) throw 'error'
-  const opts = {
-    credential: options?.credential ? options.credential : mainAccount.i,
-    origin: options?.origin ? options.origin : `${mainAccount.host}`
-  }
-  const client = useMemo(() => new api.APIClient(opts), [opts.origin, opts.credential])
-  return client
-}
+	const { mainAccount } = useLocalStorage.getState();
+	const opts = {
+		credential: options?.credential ? options.credential : mainAccount?.i,
+		origin: options?.origin ? options.origin : `${mainAccount?.host}`,
+	};
+	const client = useMemo(() => new api.APIClient(opts), [
+		opts.origin,
+		opts.credential,
+	]);
+	return client;
+};
