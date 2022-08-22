0b75aadaa9d9b69da391ac417513a67e4703f0cc
sousuke0422 <sousuke20xx@gmail.com>
2022-08-22 19:47:24 +0900

    chore: tune vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    target: [
+      'chrome100',
+      'firefox100'
+    ],
+    manifest: 'manifest.json',
+    rollupOptions: {
+      input: {
+        app: './src/main.tsx'
+      },
+      output: {
+        manualChunks: {
+          react: ['react'],
+        },
+      },
+    },
+  }
 })
