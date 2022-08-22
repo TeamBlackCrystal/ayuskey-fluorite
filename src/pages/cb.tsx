a4aa7e6c860f128d6c988b74e10cc9ae0907f6ed
yupix <yupi0982@outlook.jp>
2022-08-22 20:55:12 +0900

    chore: apiにSchemaが無かった

--- src/pages/cb.tsx
+++ src/pages/cb.tsx
@@ -2,32 +2,43 @@ import { SHA256 } from "crypto-js";
 import { FC, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { apiClient } from "strictcat";
-import { getLocalStorage, removeLocalStorage, useLocalStorage } from "../store/auth";
+import { Schema } from "../models/api";
+import {
+	getLocalStorage,
+	removeLocalStorage,
+	useLocalStorage,
+} from "../store/auth";
 
 export const CallBack: FC = () => {
-  const search = useLocation().search;
-  const query = new URLSearchParams(search);
-  const token = query.get("token")
-  const secret = getLocalStorage("_auth_secret", null)
-  const storage = useLocalStorage()
-  removeLocalStorage("_auth_secret")
+	const search = useLocation().search;
+	const query = new URLSearchParams(search);
+	const token = query.get("token");
+	const secret = getLocalStorage("_auth_secret", null);
+	const storage = useLocalStorage();
+	removeLocalStorage("_auth_secret");
 
-  useEffect(() => {
-    if (!(secret && token)) {window.location.href = '/'}
+	useEffect(() => {
+		if (!(secret && token)) {
+			window.location.href = "/";
+		}
 
-    if (secret && token && storage.mainAccount) {
-      console.log(storage.mainAccount)
-    apiClient(`${storage.mainAccount.host}`).call("POST", "/api/auth/session/userkey", {}, {appSecret: secret, token: token}).then((res) => {
-      if (res.type === 'failed') throw res.type, res.data
-      storage.setMainAccountI(SHA256(res.data.accessToken+secret).toString())
-      window.location.href = '/'
-    })
-  }
-  }, [])
+		if (secret && token && storage.mainAccount) {
+			apiClient<Schema>(`${storage.mainAccount.host}`)
+				.call(
+					"POST",
+					"/api/auth/session/userkey",
+					{},
+					{ appSecret: secret, token: token },
+				)
+				.then((res) => {
+					if (res.type === "failed") throw res.type, res.data;
+					storage.setMainAccountI(
+						SHA256(res.data.accessToken + secret).toString(),
+					);
+					window.location.href = "/";
+				});
+		}
+	}, []);
 
-  return (
-    <div>
-
-    </div>
-  )
-}
+	return <div></div>;
+};
