5616f1efc91f78f3d4b277c4d4160c674adfd0b3
yupix <yupi0982@outlook.jp>
2022-08-22 15:35:30 +0900

    feat: 通知を出すためのプロバイダーを追記

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Stream } from "@ayuskey/misskey.js";
 import { NextUIProvider } from "@nextui-org/react";
+import { SnackbarProvider } from "notistack";
 import { createContext } from "react";
 import { useStreaming } from "./hooks/webSocket";
 import { Router } from "./Route";
@@ -8,12 +9,13 @@ export const streamingContext = createContext<Stream | null>(null);
 
 export const App = () => {
 	const stream = useStreaming();
-
 	return (
 		<streamingContext.Provider value={stream}>
+      <SnackbarProvider maxSnack={3}>
 			<NextUIProvider>
 				<div style={{ width: "100vw", height: "100vh" }}><Router /></div>
 			</NextUIProvider>
+      </SnackbarProvider>
 		</streamingContext.Provider>
 	);
 };
