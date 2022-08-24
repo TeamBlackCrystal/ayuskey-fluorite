615b93eee331796e2d192cf945c8a405ef61b946
yupix <yupi0982@outlook.jp>
2022-08-24 22:15:32 +0900

    chore: commit忘れ #146

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,19 +1,24 @@
-import { FC, memo } from "react";
+import { UserDetailed } from "@ayuskey/misskey.js/built/entities";
+import { FC, memo, useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import { PageTransition } from "./components/PageTransition";
 import { CallBack } from "./pages/cb";
 import { Home } from "./pages/home";
 import { UserProfile } from "./pages/user/profile";
 import { Welcome } from "./pages/welcome";
-import { useLocalStorage } from "./store/auth";
+import { useLogin } from "./hooks/useLogin";
+import { useAsync } from "react-async"
 
 
 export const Router: FC = memo(() => {
-  const storage = useLocalStorage()
+  const login = useAsync({promiseFn: useLogin})
     return (
+      <PageTransition>
         <Routes>
-            <Route path="/" element={storage.mainAccount && storage.mainAccount.i && storage.mainAccount.host ? <Home />: <Welcome />} />
+            <Route path="/" element={login.data ? <Home />: <Welcome />} />
             <Route path="/@:username" element={<UserProfile />} />
             <Route path="/cb" element={<CallBack />} />
         </Routes>
+        </PageTransition>
     )
 })
