7e537cdf6f891fe8c7ba15a1b1f945eebe890a32
yupix <yupi0982@outlook.jp>
2022-08-25 23:07:38 +0900

    feat: themeを追加

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,23 @@
 
 import { createTheme } from "@nextui-org/react";
+import {proxy} from "valtio"
+
+export const useDarkTheme = proxy({
+  base: "dark",
+  primary: '#99CC00',
+  secondary: '#272727',
+  text: '#D5D5D6',
+  props: {
+		accent: '#63e2b7',
+		bg: '#101014',
+		fg: '#D5D5D6',
+    divider: 'rgba(255, 255, 255, 0.1)',
+		panel: '#18181c',
+		panelHeaderBg: () => useDarkTheme.props.panel,
+  }
+})
+
+export const theme = useDarkTheme
 
 export const lightTheme = createTheme({
   type: 'light',
