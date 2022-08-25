229471a11c7c9e1cfe55075664b38c69a1c4754f
yupix <yupi0982@outlook.jp>
2022-08-25 17:35:24 +0900

    feat: useSessionを追加

---
+++ src/store/session.ts
@@ -0,0 +1,10 @@
+import { LiteInstanceMetadata } from "ayuskey.js/built/entities";
+import {proxy} from "valtio"
+
+interface ISession {
+  meta: LiteInstanceMetadata | null;
+}
+
+export const useSession = proxy<ISession>({
+  meta: null,
+})
