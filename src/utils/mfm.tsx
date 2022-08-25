b38feea8414f8f5289a4fb901ddc539a2a304f77
yupix <yupi0982@outlook.jp>
2022-08-25 17:44:10 +0900

    feat: mfmを実装 close #155

--- src/utils/mfm.tsx
+++ src/utils/mfm.tsx
@@ -1,45 +1,85 @@
+import { CustomEmojiLite } from "ayuskey.js/built/entities";
 import * as mfm from "mfm-js";
-import { Link } from "react-router-dom";
+import { CSSProperties, FC, useMemo } from "react";
+import { Emoji } from "../components/common/emoji";
+interface TreeProps {
+	tree: mfm.MfmNode;
+	plain?: boolean;
+	emojis?: CustomEmojiLite[];
+}
 
-// export const MFMParse = (data: mfm.MfmNode) => {
-//   console.log(data.type)
-// 		switch (data.type) {
-// 			case "url": {
-// 				apiClient.call("GET", "/url", { query: { url: data.props.url } }).then((
-// 					res,
-// 				) => {
-// 					if (res.type === "failed") {
-// 						throw res.type, res.data;
-// 					}
-// 					const og = res.data;
-// 					return (
-// 						<Link to={og.url}>
-// 							<div
-// 								style={{
-// 									alignItems: "center",
-// 									backgroundColor: "rgba(0, 0, 0, 0.4)",
-// 									backgroundPosition: "50%",
-// 									backgroundRepeat: "no-repeat",
-// 									backgroundSize: "contain",
-//                   display: "flex",
-//                   height: "100%",
-//                   justifyContent: "center",
-//                   position: "absolute",
-//                   width: "100px"
-// 								}}
-// 							/>
-//               <article style={{left: "100px", width: "calc(100% - 100px)"}}>
-//                 <header style={{marginBottom: "8px"}}>
-//                   <h1 style={{overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap"}}>{og.title}</h1>
-//                 </header>
-//                 <p style={{overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap"}}>
-//                   {og.description}
-//                 </p>
+const Tree: FC<TreeProps> = ({ tree, emojis, plain }) => {
+	switch (tree.type) {
+		case "url": {
+			return <a href={tree.props.url} target="_blank">{tree.props.url}</a>;
+		}
+    case "text": {
+      const text = tree.props.text.replace(/(\r\n|\n|\r)/g, '\n')
+      if (plain) {
+        const res = [];
+        for (const t of text.split('\n')) {
+          res.push(<br />);
+          res.push(t);
+        }
+        res.shift();
+        return res;
+      } else {
+        return [text.replace(/\n/g, ' ')];
+      }
+    }
+    case "emojiCode": {
+      console.log(tree.props.name, emojis)
+      return <Emoji emoji={`:${tree.props.name}:`} customEmojis={emojis}/>
+    }
 
-//               </article>
-// 						</Link>
-// 					);
-// 				});
-// 		}
-//   }
-// };
+
+		default:
+			return <div className="text-red">謎 {tree.type}</div>;
+	}
+};
+
+interface ForestProps {
+	forest?: mfm.MfmNode[];
+	plain?: boolean;
+	emojis?: CustomEmojiLite[];
+}
+
+const Forest: FC<ForestProps> = ({ forest, plain, emojis }) => {
+	return forest ? (
+		<>{forest.map(
+			(tree, i) => <Tree tree={tree} key={i} emojis={emojis} plain={plain} />,
+		)}</>
+	) : null;
+};
+
+interface MFMProps {
+	plain?: boolean;
+	text: string;
+	emojis?: CustomEmojiLite[];
+  style?: CSSProperties
+}
+
+export const MFM: FC<MFMProps> = ({ text, emojis, plain,style }) => {
+	const forest = useMemo(() =>
+		plain ? mfm.parseSimple(text) : mfm.parse(text), [text, plain]);
+	const body = <Forest forest={forest} plain={plain} emojis={emojis} />;
+	return plain ? <span style={style}>{body}</span> : <span style={style}>{body}</span>;
+}
+
+export function unique<T>(xs: T[]): T[] {
+	return [...new Set(xs)];
+}
+const removeHash = (x: string) => x.replace(/#[^#]*$/, '');
+export function extractUrlFromMfm(nodes: mfm.MfmNode[], respectSilentFlag = true): string[] {
+	const urlNodes = mfm.extract(nodes, (node) => {
+		return (node.type === 'url') || (node.type === 'link' && (!respectSilentFlag || !node.props.silent));
+	});
+  //@ts-ignore
+	const urls: string[] = unique(urlNodes.map(x => x.props.url));
+
+	return urls.reduce((array, url) => {
+		const urlWithoutHash = removeHash(url);
+		if (!array.map(x => removeHash(x)).includes(urlWithoutHash)) array.push(url);
+		return array;
+	}, [] as string[]);
+}
