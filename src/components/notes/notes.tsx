a49179a8ff47348335824591d4f1f51cfbe3e234
yupix <yupi0982@outlook.jp>
2022-08-27 17:18:29 +0900

    chore: 使用されてないコンポーネントを削除

--- src/components/notes/notes.tsx
+++ src/components/notes/notes.tsx
@@ -27,37 +27,6 @@ const TIMELINES = [
 	},
 ];
 
-const Timeline = () => {
-  const stream = useSnapshot(useStream)
-  const TimelineContainer = styled.div`
-  .active: before {
-    background: ${theme.primary};
-    bottom: 0;
-    content: "";
-    display: block;
-    height: 2px;
-    left: -8px;
-    position: absolute;
-    width: calc(100% + 16px);
-  }
-  `
-  const TimelineButton = styled.button<{timeline:string}>`
-  padding: 10px;
-  font-weight: 700;
-  color: ${props => stream.currentTimeline ===  props.timeline ? theme.primary : theme.text};
-  `
-	return (
-		<TimelineContainer>
-		{TIMELINES.map(
-			(timeline) => (
-				<TimelineButton key={timeline.name} timeline={timeline.name} className={`${stream.currentTimeline ===  timeline.name ? 'active': ''}`}>
-					{timeline.element}
-				</TimelineButton>
-			),
-		)}
-		</TimelineContainer>
-	);
-};
 export const Notes: FC = () => {
 	const { notes } = useSnapshot(useNotes) as typeof useNotes;
 
