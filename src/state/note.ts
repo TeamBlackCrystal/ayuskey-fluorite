79568656c6fcb65d5d1462ff26365f8206eff00a
yupix <yupi0982@outlook.jp>
2022-08-27 17:22:23 +0900

    feat: noteの取得中を状態に追加

--- src/state/note.ts
+++ src/state/note.ts
@@ -3,11 +3,17 @@ import {proxy} from "valtio"
 
 type Notes = {
 	notes: Note[];
+  fetchNote: boolean,
+  changeFetchNode: (status: boolean) => void
   addNote: (note: Note) => void
 };
 
 export const useNotes = proxy<Notes>({
   notes: [],
+  fetchNote: false,
+  changeFetchNode(status) {
+      useNotes.fetchNote = status
+  },
   addNote: (note: Note) => {
     useNotes.notes = [note, ...useNotes.notes]
   }
