d6ab55cdfeaf8ae9808845c1716e26e964450557
yupix <yupi0982@outlook.jp>
2022-09-02 12:01:38 +0900

    fix: ログインしてなくてもしてても動作が変わらない

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,24 +6,19 @@ import { useSnapshot } from "valtio";
 import Welcome from "~/pages/welcome";
 import { useAuth } from "~/state/auth";
 
-export const loader: LoaderFunction = ({request}) => {
-
-    return json({FRONT_URL: process.env.FRONT_URL})
-}
+export const loader: LoaderFunction = () => {
+	return json({ FRONT_URL: process.env.FRONT_URL });
+};
 
 export default function Index() {
-    const data = useLoaderData<{a: boolean}>()
-    console.log(data)
-	const accounts = useSnapshot(useAuth);
-    console.log(accounts.account, 'valtio')
+	const { account } = useSnapshot(useAuth);
 	useEffect(() => {
 		// addAccount({id: "w", "token": "NOZYFIB0sewHIpZi"})
 		// setUser({email: 'unko', name: 'aa'})
 	}, []);
-
-    if (!accounts) {
-        return <Welcome />
-    }
+    if (!account) {
+		return <Welcome />;
+	}
 
 	return (
         <Welcome />
