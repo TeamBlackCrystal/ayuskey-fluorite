23173d2fd64e8dd4468fb0841fe2b255c41a4aad
yupix <yupi0982@outlook.jp>
2022-09-03 22:22:49 +0900

    chore: indexのmetaを定義

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,21 @@
-import type { LoaderFunction } from "@remix-run/node";
-import { json } from "remix-utils";
+import type { MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { useSnapshot } from "valtio";
 import { Home } from "~/pages/home";
 import Welcome from "~/pages/welcome";
 import { useAuth } from "~/state/auth";
+import { serverSideAPI } from "~/utils/api";
 
-export const loader: LoaderFunction = () => {
-	return json({ FRONT_URL: process.env.FRONT_URL });
+
+export const meta: MetaFunction<typeof loader> = ({data}) => {
+  return {
+    title: data.SITE_NAME || 'Ayuskey Fluorite',
+  }
+}
+
+export async function loader() {
+  const meta = await serverSideAPI.request('meta')
+	return json({ SITE_NAME: process.env.SITE_NAME, meta });
 };
 
 export default function Index() {
